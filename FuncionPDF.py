388e898c72ff34be4c1ea67da7be2fdd357a9530
kevincuh <kevin.jaramillo@cuh.mx>
2023-07-25 10:25:58 -0600

    Listado de datos estaticos

--- FuncionPDF.py
+++ FuncionPDF.py
@@ -2,8 +2,16 @@ from reportlab.pdfgen import canvas
 ruta = "C:/Users/Kevo/Escritorio/Modularidad python/prueba funciones/"
 nombreArchivo = ruta + "reporteGlobal.pdf"
 
-def generarPDF():
+def generarPDF(listaNombres, listaEdades):
     c = canvas.Canvas(nombreArchivo)
-    c.drawString(200,600,"Hola desde una función")
+    xInicial = 200
+    yInicial = 700
+    c.setFont('Helvetica', 20)
+    c.drawString(xInicial,yInicial,"Edad")
+    c.drawString(xInicial + 120,yInicial,"Nombre")
+    for i in range(len(listaNombres)):
+        yInicial = yInicial - 20
+        c.drawString(xInicial,yInicial,listaEdades[i])
+        c.drawString(xInicial+120,yInicial,listaNombres[i])
     c.save()
     print("Reporte generado----------")
